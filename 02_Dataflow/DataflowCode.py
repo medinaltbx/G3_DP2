f03059eb55f611a942232061170e0e5f267ef703
medinazorin_desk <medinazorin@gmail.com>
2022-03-06 11:46:19 +0100

    Heading to end

---
+++ 02_Dataflow/DataflowCode.py
@@ -0,0 +1,104 @@
+# Dataflow EDEM Code
+
+# Import Libraries
+
+import json
+import logging
+import time
+import apache_beam as beam
+from apache_beam.options.pipeline_options import (PipelineOptions, StandardOptions)
+from apache_beam.transforms.combiners import MeanCombineFn
+from apache_beam.transforms.combiners import CountCombineFn
+from apache_beam.transforms.core import CombineGlobally
+import apache_beam.transforms.window as window
+from apache_beam.io.gcp.bigquery import parse_table_schema_from_json
+from apache_beam.io.gcp import bigquery_tools
+import datetime
+
+# ParseJson Function
+
+# Get data from PubSub and parse them
+
+def parse_json_message(message):
+    # Mapping message from PubSub
+    # DecodePubSub message in order to deal with
+    pubsubmessage = message.data.decode('utf-8')
+    # Get messages attributes
+    attributes = message.attributes
+
+    # Print through console and check that everything is fine.
+    logging.info("#~#~#~#~#~#~#~#~#~#~#~# Receiving message from PubSub:%s #~#~#~#~#~#~#~#~#~#~#~#", message)
+    logging.info("with attributes: %s", attributes)
+
+    # Convert string decoded in json format(element by element)
+
+    row = json.loads(pubsubmessage)
+    logging.info(" ################ MENSAJITO json: %s ################: ", row)
+
+    # Return function
+    return row
+
+
+class parse_json(beam.DoFn):
+    def process(self, element):
+        output_json = json.dumps(element)
+        yield output_json.encode('utf-8')
+
+# class filter_departures(beam.DoFn):
+#     def process(self, element):
+#         if (element['status'] == 'salida'):
+#             yield element
+#         else:
+#             yield element
+
+
+# Create Beam pipeline
+
+def edemData(output_table, project_id):
+    # Load schema from BigQuery/schemas folder
+    with open(f"schemas/{output_table}.json") as file:
+        input_schema = json.load(file)
+
+    # Declare bigquery schema
+    schema = bigquery_tools.parse_table_schema_from_json(json.dumps(input_schema))
+
+    # Create pipeline
+    # First of all, we set the pipeline options
+    options = PipelineOptions(save_main_session=True, streaming=True)
+    with beam.Pipeline(options=options) as p:
+        # Part01: we create pipeline from PubSub to BigQuery
+
+        data = (
+            # Read messages from PubSub
+                p | "Read messages from PubSub" >> beam.io.ReadFromPubSub(
+            subscription=f"projects/{project_id}/subscriptions/{output_table}-sub", with_attributes=True)
+                # Parse JSON messages with Map Function and adding Processing timestamp
+                | "Parse JSON messages" >> beam.Map(parse_json_message)
+        )
+
+        # Part02: Write proccessing message to their appropiate sink
+        # Data to Bigquery
+
+        (data | "Write to BigQuery" >> beam.io.WriteToBigQuery(
+            table=f"{project_id}:edemDataset.{output_table}",
+            schema=schema,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+        ))
+
+        # Part03: Filter and publish data
+
+        (data
+            | "Parse JSON" >> beam.ParDo(parse_json())
+            | "WriteToPubSub" >> beam.io.WriteToPubSub(topic=f"projects/{project_id}/topics/IotToCloudFunctions", with_attributes=False)
+         )
+
+        #(data
+        #    | "Filter messages" >> beam.Filter(lambda element: element['status'] == 'salida')
+        #    | "WriteToPubSub" >> beam.io.WriteToPubSub(topic=f"projects/{project_id}/topics/IotToCloudFunctions",
+        #                                                with_attributes=False)
+        #)
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    edemData("iotToBigQuery", "dp2-test-342416")
---
+++ 03_CloudFunctions/calculate_time_cloud.py
@@ -0,0 +1,67 @@
+# Cloud Function triggered by PubSub Event
+# When a temperature over 23ºC or under 17ºC is received, a IoT Core command will be throw.
+
+# Import libraries
+import random
+from datetime import datetime
+from google.cloud import bigquery
+import base64, json, sys, os
+import google.cloud.logging
+import logging
+import pandas as pd
+
+# Read from PubSub
+def calculate_time(event, context):
+    client = google.cloud.logging.Client()
+    client.setup_logging()
+
+    # Read message from Pubsub (decode from Base64)
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+
+    # Load json
+    message = json.loads(pubsub_message)
+
+    logging.info(message)
+    logging.debug(message)
+
+    if message['status'] == 'salida':
+        print('MESSAge: ', message)
+        TABLE_READ = "`dp2-test-342416.edemDataset.iotToBigQuery`"
+        TABLE_DESTINATION ="dp2-test-342416.edemDataset.status"
+
+        def parse_time(timestamp):
+            return datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+
+        departure_time = parse_time(message['timeStamp'])
+
+        # Construct a BigQuery client object.
+        client = bigquery.Client()
+        """ READ BIGQUERY STATUS"""
+        q = f"""SELECT * FROM {TABLE_READ} WHERE parking_id = '{message['parking_id']}' AND status = 'llegada'"""
+        df = (client.query(q).result().to_dataframe())
+
+        """ GET LAST ARRIVAL """
+        df['timeStamp'] = df['timeStamp'].apply(parse_time)
+        df = df[df['timeStamp']<departure_time].sort_values(by=['timeStamp'])
+        arrival_time = df['timeStamp'].iloc[-1]
+
+        ellapsed_time = departure_time - arrival_time
+        precio = random.uniform(1.5, 1.9)
+
+        status = [{'parking_id': message['parking_id'],
+                    'arrival_time': str(arrival_time),
+                    'departure_time': str(departure_time),
+                    'total_time': str(ellapsed_time),
+                    'combustible':message['combustible'],
+                    'marca':message['marca'],
+                    'matricula':message['matricula'],
+                    'precio':precio}]
+
+        bq_client = bigquery.Client()
+
+        table = bq_client.get_table(TABLE_DESTINATION)
+        errors = bq_client.insert_rows_json(table, status)
+        if errors == []:
+            logging.info(" #~#~#~#~#~#~# SUCCESS #~#~#~#~#~#~#")
+        else:
+            logging.info(" #~#~#~#~#~#~# FALLO #~#~#~#~#~#~#")
